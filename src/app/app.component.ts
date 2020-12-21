a6626a5d1cbc210f0cc743a37b4f8db9a8c452fb
Jesus Manuel Loredo Sauceda <manuel.lorsau@gmail.com>
2020-12-21 13:55:48 -0700

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { promise } from 'protractor';
+import { resolve } from 'dns';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+   nombre: string = 'Capitán América';
+   nombre2: string = 'jeSuS mAnUeL lOrEdO sAUcEda';
+   personajes: string[] = ['Iroman', 'Spiderman', 'Thor', 'Loki', 'Groot'];
+   PI: number = Math.PI;
+   porcentaje: number = .2199;
+   salario: number = 1234.5;
+  fecha: Date = new Date();
+  idioma: string;
+  videoUrl: string = 'https://www.youtube.com/embed/hpBBvzrtr60';
+  activar: boolean = true;
+  valorPromesa = new Promise<string>( ( resolve ) => {
+      setTimeout(() => {
+        resolve('llego la data');
+      }, 4500);
+  });
+
+   heroe =  {
+nombre : 'Logan',
+clave: 'Wolverine',
+edad: 500,
+direccion: {
+  calle: 'Principal',
+casa: 20,
+}
+   };
 }
